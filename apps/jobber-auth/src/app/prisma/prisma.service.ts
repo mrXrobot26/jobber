c4b815e17af8ae5b9d89a3c192207df4c4ee2345
mrXrobot26 <zezoelbessa10@gmail.com>
2025-06-03 11:42:47 +0300

    Add PostgreSQL setup with Prisma integration and initial schema

---
+++ apps/jobber-auth/src/app/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma-clients/jobber-auth';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
